06f3c3f7020e1f51931b2ef49d590f26da27f9c2
Pedrodotcom <pedroluisaantonio@gmail.com>
2020-07-05 02:05:04 -0300

    Register Origin

---
+++ frontend/public/js/services-selection.js
@@ -0,0 +1,23 @@
+let selectedServices = [];
+const services = document.querySelectorAll('.grid-services li');
+const inputServices = document.querySelector('[name=services]');
+
+    for(service of services) {
+        service.onclick = event => {
+            const serviceE = event.target;
+                serviceE.classList.toggle('selected');
+
+
+            const serviceId = serviceE.dataset.id;
+                if(selectedServices.indexOf(serviceId) == -1) {
+                    selectedServices.push(serviceId);
+                    console.log(selectedServices)
+                } else {
+                    selectedServices.splice(selectedServices.indexOf(serviceId), 1);
+                    console.log(selectedServices)
+
+                };
+
+            inputServices.value = selectedServices.sort();
+        };
+    };
